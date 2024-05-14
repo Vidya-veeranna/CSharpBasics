4ef06f665ab6ca1605bf71110c000eb73e86d1ee
Vidya-veeranna <vidyagowda191@gmail.com>
2024-05-14 22:08:16 +0530

    optional parameter

---
+++ FundamentalPrograms/OptionalParameters.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var ex1 = new optionalParam();
+ex1.printOptionalParam();
+Console.ReadKey();
+class optionalParam
+{
+    public string Name;
+    public int Age;
+
+    public optionalParam(string name="unknown", int age =20)
+    {
+        Name = name;
+        Age = age;
+    }
+    public void printOptionalParam()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Äge: " + Age);
+    }
+}
